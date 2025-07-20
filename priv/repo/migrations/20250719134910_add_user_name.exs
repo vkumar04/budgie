9f85afd833028e83875299f3be39b020c47965ee
Vick Kumar <vick.kumar44@gmail.com>
2025-07-20 08:27:31 -0400

    added db migration for name

---
+++ priv/repo/migrations/20250719134910_add_user_name.exs
@@ -0,0 +1,10 @@
+defmodule Budgie.Repo.Migrations.AddUserName do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+    end
+  end
+end
